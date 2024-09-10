462b7742171936a0f08f82b4cbff70fcaba878ee
alona007 <alona.olyksashenko@gmail.com>
2024-09-10 10:28:18 +0200

    hw3

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -1,23 +1,23 @@
 import express from 'express';
-import { ContactsCollection } from '../models/contacts.js';
+import {
+  getAllContacts,
+  getContactById,
+  createContact,
+  updateContact,
+  deleteContact,
+} from '../controllers/contacts.js';
+import ctrlWrapper from '../utils/ctrlWrapper.js';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  try {
-    const contacts = await ContactsCollection.find();
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  } catch (error) {
-    console.error('Error fetching contacts:', error);
-    res.status(500).json({
-      status: 500,
-      message: 'Internal server error',
-    });
-  }
-});
+router.get('/', ctrlWrapper(getAllContacts));
+
+router.get('/:contactId', ctrlWrapper(getContactById));
+
+router.post('/', ctrlWrapper(createContact));
+
+router.patch('/:contactId', ctrlWrapper(updateContact));
+
+router.delete('/:contactId', ctrlWrapper(deleteContact));
 
 export default router;
