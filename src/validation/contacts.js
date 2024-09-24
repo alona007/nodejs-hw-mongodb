fa3dcadcca754f0a807990df9f78941bc563787e
alona007 <alona.olyksashenko@gmail.com>
2024-09-24 21:53:21 +0200

    fix

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -4,7 +4,7 @@ import { contactTypeList } from '../constants/contacts.js';
 export const ContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().min(10).max(13).required(),
-  email: Joi.string().email().required(),
+  email: Joi.string().email(),
   age: Joi.number().integer().min(6).max(16),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid(...contactTypeList),
